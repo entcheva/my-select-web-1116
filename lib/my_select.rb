48737d8a5302dc5c7c88c4f860af6f57e00204ab
Christina Entcheva <christina.entcheva@gmail.com>
2016-11-20 18:51:06 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ arr = []
+
+   if collection.empty?
+     arr
+   else
+     while i < collection.length
+       if yield collection[i]
+         arr.push(collection[i])
+       end
+       i += 1
+     end
+   end
+   arr
 end
